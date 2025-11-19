340a21c46b43bb6cac8070196a8813b23549fa96
SamCalliskan <erdincscaliskan@gmail.com>
2025-11-19 23:50:30 +0300

    Update sccratch_.py

--- sccratch_.py
+++ sccratch_.py
@@ -1,6 +1,4 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
+
 ... import random
 ... import time
 ... 
@@ -51,4 +49,5 @@ Enter "help" below or click "Help" above for more information.
 
 if __name__ == "__main__":
     main()
+
 
